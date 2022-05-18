6a00d58c7f4a7cd1d96dd626f5bdd4b50186aeb1
Punit <puneethkumarvh@gmail.com>
2022-05-18 21:29:13 +0530

    Done with validation of friends table

--- app/models/friend.rb
+++ app/models/friend.rb
@@ -2,4 +2,7 @@
 
 class Friend < ApplicationRecord
   belongs_to :user
+  validates :first_name, length: { minimum: 3 }, presence: true
+  validates :phone, numericality: true, length: { is: 10 }
+  validates :email, format: { with: URI::MailTo::EMAIL_REGEXP } 
 end
